eda831001b087a1d257fc4e34980d58cfe48bf77
Cole Odegard <codegard@carroll.edu>
2025-02-18 11:47:16 -0700

    Task parallelization with workers in a list of workers

--- src/Plant.java
+++ src/Plant.java
@@ -1,4 +1,3 @@
-import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
 
@@ -9,20 +8,16 @@ public class Plant implements Runnable {
     //    public volatile boolean betterGuitarPlayer = false;
     //    public static volatile boolean redGreenColorBlind = true;
 
-    private static final int NUM_PLANTS = 3;
+    private static final int NUM_PLANTS = 1;
+    private static final int NUM_WORKERS = 6; // THIS MUST BE AT LEAST 4 WORKERS
 
-    private final BlockingQueue<Orange> fetchMailbox = new LinkedBlockingQueue<Orange>();
+    private final BlockingQueue<Orange> fetchMailbox = new LinkedBlockingQueue<>();
     private final BlockingQueue<Orange> peelMailbox = new LinkedBlockingQueue<>();
     private final BlockingQueue<Orange> squeezeMailbox = new LinkedBlockingQueue<>();
     private final BlockingQueue<Orange> bottleMailbox = new LinkedBlockingQueue<>();
     private final BlockingQueue<Orange> processedMailbox = new LinkedBlockingQueue<>();
 
-    private final Worker fetchWorker;
-    private final Worker peelWorker;
-    private final Worker peelWorker2;
-    private final Worker squeezeWorker;
-    private final Worker squeezeWorker2;
-    private final Worker bottleWorker;
+    private final Worker[] workers = new Worker[NUM_WORKERS];
 
 
     public static void main(String[] args) {
@@ -56,8 +51,10 @@ public class Plant implements Runnable {
             totalWasted += p.getWaste();
         }
         System.out.println("Total provided/processed = " + totalProvided + "/" + totalProcessed);
+        System.out.println();
         System.out.println("Created " + totalBottles +
                 " bottles, wasted " + totalWasted + " oranges");
+        System.out.println();
     }
 
     private static void delay(long time, String errMsg) {
@@ -77,42 +74,39 @@ public class Plant implements Runnable {
     private int orangesProcessed;// workers come in "teams" of 7
     private volatile boolean timeToWork; // ensures that the value is written when written and read when read -- only needed across threads
     // if the value is modified within a thread the value is stored in a thread's cache and not written to main memory
+    private final String[] jobs = new String[]{"Fetch", "Peel", "Squeeze", "Bottle", "Peel", "Squeeze"};
+    private final BlockingQueue<Orange>[] inputQueues = new BlockingQueue[]{fetchMailbox, peelMailbox, squeezeMailbox, bottleMailbox, peelMailbox, squeezeMailbox};
+    private final BlockingQueue<Orange>[] outputQueues = new BlockingQueue[]{peelMailbox, squeezeMailbox, bottleMailbox, processedMailbox, squeezeMailbox, bottleMailbox};
 
     Plant(int threadNum) {
         orangesProvided = 0;
         orangesProcessed = 0;
         // initialize each worker
         thread = new Thread(this, "Plant[" + (threadNum + 1) + "]");
-        fetchWorker = new Worker("Fetch", fetchMailbox, peelMailbox);
-        peelWorker = new Worker("Peel", peelMailbox, bottleMailbox);
-        peelWorker2 = new Worker("Peel2", peelMailbox, bottleMailbox);
-        squeezeWorker = new Worker("Squeeze", squeezeMailbox, bottleMailbox);
-        squeezeWorker2 = new Worker("Squeeze2", squeezeMailbox, bottleMailbox);
-        bottleWorker = new Worker("Bottle", bottleMailbox, processedMailbox);
+        for (int i = 0; i < NUM_WORKERS; i++) {
+            // find out which queues and what job the worker will have / pull from
+            int index = i % 6;
+//            System.out.println("Creating worker " + jobs[index]);
+            workers[i] = new Worker(jobs[index], inputQueues[index], outputQueues[index]);
+        }
     }
 
     public void startPlant() {
         // start all the workers and the main plant thread
         timeToWork = true;
         thread.start();
-        fetchWorker.startWork();
-        peelWorker.startWork();
-        peelWorker2.startWork();
-        squeezeWorker.startWork();
-        squeezeWorker2.startWork();
-        bottleWorker.startWork();
+        for (Worker w : workers) {
+            w.startWork();
+        }
     }
 
 
     public void stopPlant() {
         // stop all the workers
         timeToWork = false;
-        fetchWorker.stopWork();
-        peelWorker.stopWork();
-        peelWorker2.stopWork();
-        squeezeWorker.stopWork();
-        squeezeWorker2.stopWork();
-        bottleWorker.stopWork();
+        for (Worker w : workers) {
+            w.stopWork();
+        }
     }
 
     public void waitToStop() {
@@ -126,16 +120,10 @@ public class Plant implements Runnable {
     public void run() {
         System.out.println(Thread.currentThread().getName() + " Processing oranges");
         while (timeToWork) {
-            // start a new orange by putting it in the fetchMailbox
-            try {
+            if(fetchMailbox.isEmpty()) {
                 Orange orange = new Orange();
-                // since the minimum amount of time for a worker to fetch an orange is 15ms we need to wait that long
-                // to put an orange in the queue.
-                thread.sleep(15);
-                fetchMailbox.add(orange);
                 orangesProvided++;
-//                System.out.print(".");
-            } catch (InterruptedException ignored) {
+                fetchMailbox.add(orange);
             }
         }
         System.out.println();
