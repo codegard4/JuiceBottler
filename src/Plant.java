d6a6895457113cef76b001137bb71af6116bbd2e
Cole Odegard <codegard@carroll.edu>
2025-02-17 21:54:50 -0700

    Task paralellization but running into deadlock

--- src/Plant.java
+++ src/Plant.java
@@ -5,8 +5,19 @@ public class Plant implements Runnable {
     //    private static volatile boolean wearingFuzzyPinkBunnySlippers = false;
     //    public volatile boolean betterGuitarPlayer = false;
     //    public static volatile boolean redGreenColorBlind = true;
-    private static final int NUM_WORKERS = 1;
+
     private static final int NUM_PLANTS = 3;
+
+    private final BlockingMailbox fetchMailbox = new BlockingMailbox();
+    private final BlockingMailbox peelMailbox = new BlockingMailbox();
+    private final BlockingMailbox squeezeMailbox = new BlockingMailbox();
+    private final BlockingMailbox bottleMailbox = new BlockingMailbox();
+    private final BlockingMailbox processedMailbox = new BlockingMailbox();
+
+    private final Worker fetchWorker;
+    private final Worker peelWorker;
+    private final Worker squeezeWorker;
+    private final Worker bottleWorker;
 
 
     public static void main(String[] args) {
@@ -24,9 +35,9 @@ public class Plant implements Runnable {
         for (Plant p : plants) {
             p.stopPlant();
         }
-        for (Plant p : plants) {
-            p.waitToStop();
-        }
+//        for (Plant p : plants) {
+//            p.waitToStop();
+//        }
 
         // Summarize the results
         int totalProvided = 0;
@@ -58,8 +69,7 @@ public class Plant implements Runnable {
     // each thread represents 1 worker completing their juice bottling tasks
     private final Thread thread;
     private int orangesProvided;
-    private int orangesProcessed;
-    private Worker[] workers = new Worker[NUM_WORKERS * 7]; // workers come in "teams" of 7
+    private int orangesProcessed;// workers come in "teams" of 7
     private volatile boolean timeToWork; // ensures that the value is written when written and read when read -- only needed across threads
     // if the value is modified within a thread the value is stored in a thread's cache and not written to main memory
 
@@ -68,26 +78,30 @@ public class Plant implements Runnable {
         orangesProcessed = 0;
         // initialize each worker (thread)
         thread = new Thread(this, "Plant[" + (threadNum + 1) + "]");
-        for (int i = 0; i < NUM_WORKERS; i++) {
-            workers[i] = new Worker(i);
-        }
+        fetchWorker = new Worker(threadNum, "Fetch", fetchMailbox, peelMailbox);
+        peelWorker = new Worker(threadNum, "Peel", peelMailbox, bottleMailbox);
+        squeezeWorker = new Worker(threadNum, "Squeeze", squeezeMailbox, bottleMailbox);
+        bottleWorker = new Worker(threadNum, "Bottle", bottleMailbox, processedMailbox);
     }
 
     public void startPlant() {
         timeToWork = true;
         thread.start();
-        for (Worker w : workers) {
-            w.startWork();
-        }
+        fetchWorker.startWork();
+        peelWorker.startWork();
+        squeezeWorker.startWork();
+        bottleWorker.startWork();
     }
 
 
     public void stopPlant() {
-        for (Worker w : workers) {
-            w.stopWork();
-        }
-
+        System.out.println("Stopping Plant");
         timeToWork = false;
+        fetchWorker.stopWork();
+        peelWorker.stopWork();
+        squeezeWorker.stopWork();
+        bottleWorker.stopWork();
+        System.out.println("Plant stopped");
     }
 
     public void waitToStop() {
@@ -102,13 +116,11 @@ public class Plant implements Runnable {
     public void run() {
         System.out.println(Thread.currentThread().getName() + " Processing oranges");
         while (timeToWork) {
-            for (Worker w : workers) {
-                BlockingMailbox mailbox = new BlockingMailbox();
-                mailbox.put(new Orange());
-
+            // start a new orange by putting it in the fetchMailbox
+                Orange orange = new Orange();
+                fetchMailbox.put(orange);
                 orangesProvided++;
                 System.out.print(".");
-            }
         }
         System.out.println();
         System.out.println(Thread.currentThread().getName() + " Done");
@@ -119,14 +131,18 @@ public class Plant implements Runnable {
     }
 
     public int getProcessedOranges() {
+        while(!processedMailbox.isEmpty()) {
+            processedMailbox.get();
+            orangesProcessed++;
+        }
         return orangesProcessed;
     }
 
     public int getBottles() {
-        return orangesProcessed / ORANGES_PER_BOTTLE;
+        return getProcessedOranges() / ORANGES_PER_BOTTLE;
     }
 
     public int getWaste() {
-        return orangesProcessed % ORANGES_PER_BOTTLE;
+        return getProcessedOranges() % ORANGES_PER_BOTTLE;
     }
 }
--- src/Worker.java
+++ src/Worker.java
@@ -3,77 +3,63 @@ public class Worker implements Runnable {
 
 
     public static void main(String[] args) {
-    // we run the workers from the plant class
+        // we run the workers from the plant class
     }
 
-//    Fetched(15),
+    //    Fetched(15),
 //    Peeled(38), // Peeled takes more than twice as much time as fetched
 //    Squeezed(29), // Squeezed takes a long time too
 //    Bottled(17),
 //    Processed(1);
-    public final int ORANGES_PER_BOTTLE = 3;
 
     // each thread represents 1 worker completing their juice bottling tasks
     private final Thread thread;
-    private final String[] Jobs = new String[]{"Fetched", "Peeled", "Squeezed", "Bottled", "Processed", "Peeled", "Squeezed"};
-    private int orangesProvided;
-    private int orangesProcessed;
-    private String job;
-    private volatile boolean timeToWork; // ensures that the value is written when written and read when read -- only needed across threads
+    // save the job that a worker should be doing as a string
+    private final String job;
+    // workers can do multiple tasks, or in my very monotenous (and simple factory) they will do one task
+    // here is the queue of oranges they get
+    private final BlockingMailbox orangesToProcess;
+    private final BlockingMailbox orangesProcessed;
+    private volatile boolean timeToWork;
+
     // if the value is modified within a thread the value is stored in a thread's cache and not written to main memory
 
-    Worker(int threadNum) {
-        orangesProvided = 0;
-        orangesProcessed = 0;
-        // initialize each worker (thread)
-        job = Jobs[threadNum % 7];
+    Worker(int threadNum, String job, BlockingMailbox orangesToProcess, BlockingMailbox orangesProcessed) {
+
+        // initialize each worker -- both queues should be tied to another worker
+        this.orangesToProcess = orangesToProcess;
+        this.orangesProcessed = orangesProcessed;
+        this.job = job;
         System.out.println("Worker " + threadNum + " job: " + job);
-        thread = new Thread(this, " Worker[" + (threadNum+1) + "]");
+        thread = new Thread(this, " Worker[" + (threadNum + 1) + "]");
     }
 
-    public String job() { return job; }
+    public String job() {
+        return job;
+    }
 
     public void startWork() {
         timeToWork = true;
         // start each worker on their juice bottling tasks
 
-            thread.start();
+        thread.start();
     }
 
     public void stopWork() {
         timeToWork = false;
+        thread.interrupt();
     }
 
     public void run() {
         while (timeToWork) {
-            processEntireOrange(new Orange());
-            orangesProvided++;
+            System.out.println(job + "ing Being done");
+            Orange orange = orangesToProcess.get(); // get or wait until you can get an orange
+            orange.runProcess(); // do something with the orange
+            orangesProcessed.put(orange); // done with the orange, give it to the next worker!
             System.out.print(".");
         }
         System.out.println();
         System.out.println(Thread.currentThread().getName() + " Done");
     }
 
-    public void processEntireOrange(Orange o) {
-        while (o.getState() != Orange.State.Bottled) {
-            o.runProcess();
-        }
-        orangesProcessed++;
-    }
-
-    public int getProvidedOranges() {
-        return orangesProvided;
-    }
-
-    public int getProcessedOranges() {
-        return orangesProcessed;
-    }
-
-    public int getBottles() {
-        return orangesProcessed / ORANGES_PER_BOTTLE;
-    }
-
-    public int getWaste() {
-        return orangesProcessed % ORANGES_PER_BOTTLE;
-    }
 }
